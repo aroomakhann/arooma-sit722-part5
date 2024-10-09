a502d88bdab2e3814011a43a7b77c25759b990d2
Arooma <aroomakhan1@outlook.com>
2024-10-09 20:06:56 +1100

    issue fix

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -x
 
-: "$CONTAINER_REGISTRY"
+: "${CONTAINER_REGISTRY:=arooma123acr.azurecr.io}"
 : "$VERSION"
 : "$REGISTRY_UN"
 : "$REGISTRY_PW"
 
+# Login to the Azure Container Registry
 echo "$REGISTRY_PW" | docker login "$CONTAINER_REGISTRY" --username "$REGISTRY_UN" --password-stdin
 
+# Push bookcatalog and inventorymanage images
 docker push "$CONTAINER_REGISTRY/bookcatalog:$VERSION"
 docker push "$CONTAINER_REGISTRY/inventorymanage:$VERSION"
 
 echo "Docker images pushed: $CONTAINER_REGISTRY/bookcatalog:$VERSION and $CONTAINER_REGISTRY/inventorymanage:$VERSION"
-
