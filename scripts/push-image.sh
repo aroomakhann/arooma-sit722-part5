cc9fe7fbbace34d725fe7ec7f8b66f8e23178095
aroomakhann <163081835+aroomakhann@users.noreply.github.com>
2024-10-09 21:19:33 +1100

    Update push-image.sh

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -6,11 +6,9 @@ set -x
 : "$REGISTRY_UN"
 : "$REGISTRY_PW"
 
-# Login to the Azure Container Registry
 echo "$REGISTRY_PW" | docker login "$CONTAINER_REGISTRY" --username "$REGISTRY_UN" --password-stdin
 
-# Push bookcatalog and inventorymanage images
 docker push "$CONTAINER_REGISTRY/bookcatalog:$VERSION"
 docker push "$CONTAINER_REGISTRY/inventorymanage:$VERSION"
 
-echo "Docker images pushed: $CONTAINER_REGISTRY/bookcatalog:$VERSION and $CONTAINER_REGISTRY/inventorymanage:$VERSION"
+echo "The images are pushed"
